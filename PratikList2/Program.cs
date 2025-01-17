74966bebf1362f173e22c496612b38ef3ebb6b20
erkankaradag <erkankaradagcom@gmail.com>
2025-01-17 14:48:48 +0300

    Add project files.

---
+++ PratikList2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace PratikList2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sevilenKahveler = new List<string>(); // Boş bir liste oluşturuluyor.
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Lütfen sevdiğiniz kahve isimlerini girin:");
+                string kahve = Console.ReadLine();      // Kullanıcının girdiği değeri alalım
+                sevilenKahveler.Add(kahve);     // Girilen kahveyi listeye ekleyelim
+            }
+            // Listeyi kontrol etmek için ekrana yazdıralım
+            Console.WriteLine("Sevilen kahveler: ");
+            foreach (string kahve in sevilenKahveler)
+            {
+                Console.WriteLine(kahve); // Her bir kahve ismini yazdıralım
+            }
+        }
+    }
+}
